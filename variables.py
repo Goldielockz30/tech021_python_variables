437e20ac3d03d4d1a50b80be34d58498fb2a5298
Goldielockz30 <ahimbajohnson@hotmail.co.uk>
2023-01-27 16:01:09 +0000

    python

---
+++ variables.py
@@ -0,0 +1,45 @@
+# Variables
+
+# variable_name = variable_data
+
+a = 1
+b = 2
+c = 3.5
+hi = "Hello World"
+
+#ctrl+/ to comment out multiple lines
+
+# print(hi)
+# print(a)
+
+# print(1 + 2)
+# print(a + b)
+
+# the type method
+
+#print(type(b))
+
+# Overwriting variables
+
+# print(a)
+# a = 4
+# print(a)
+
+#Getting user input
+
+# print("Hi, what is your name?")a
+# name = input() #this is a method
+# print("hi")
+# print(name)
+
+# Make ea program that takes the users name, age and DOB and prints back out
+# When you are done , push to GitHub and share in chat.
+
+print("Hi, what is your username?")
+username = input()
+print("How old are you?")
+age = input()
+print("What is you DOB?")
+DOB = input()
+
+print("Hi, " + username + " You are " + age + " years old and your DOB is " + DOB)
